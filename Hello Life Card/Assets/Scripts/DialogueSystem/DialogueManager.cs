0de5641a54d1ab2e2a69487be5fd8bb9f6d6872d
Weiyan Zhu <weiyanz@uci.edu>
2019-02-19 15:31:54 -0800

    Dialogue Basic

---
+++ Hello Life Card/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public GameObject dialogueBox;
+    public Text dialogueText;
+    public Dialogue dialogue;
+    
+    void Start()
+    {
+        StartCoroutine(DisplayOneSentence(dialogue));
+    }
+
+    public IEnumerator DisplayOneSentence(Dialogue dialogue)
+    {
+
+        for(int x = 1; x<= dialogue.content.Length;++x)
+        {
+            yield return null;
+            dialogueText.text = dialogue.name + "\n" + dialogue.content.Substring(0, x)
+                                + "<color=#ffffff00>" + dialogue.content.Substring(x) + "</color>";
+        }
+        dialogueText.text = dialogue.name + "\n" + dialogue.content;
+    }
+
+}
