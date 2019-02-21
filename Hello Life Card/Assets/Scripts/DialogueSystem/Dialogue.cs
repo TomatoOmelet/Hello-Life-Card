6fc2ca6d79ba247d0b8042d641afbdf1c415fa4e
Weiyan Zhu <weiyanz@uci.edu>
2019-02-20 21:17:40 -0800

    more detail about Dialogue Manager

--- Hello Life Card/Assets/Scripts/DialogueSystem/Dialogue.cs
+++ Hello Life Card/Assets/Scripts/DialogueSystem/Dialogue.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
-    public string name;
+    public string speaker;
     public string content;
+
+    public Dialogue(string speaker, string content)
+    {
+        this.speaker = speaker;
+        this.content = content;
+    }
 }
--- Hello Life Card/Assets/Scripts/DialogueSystem/DialogueManager.cs
+++ Hello Life Card/Assets/Scripts/DialogueSystem/DialogueManager.cs
@@ -11,19 +11,63 @@ public class DialogueManager : MonoBehaviour
     
     void Start()
     {
-        StartCoroutine(DisplayOneSentence(dialogue));
+        ShowSentence(new Dialogue("", "adqd"));
     }
 
-    public IEnumerator DisplayOneSentence(Dialogue dialogue)
+    //all showSentence will start the DisplayMultipleSentencesRoutine
+    public void ShowSentence(Dialogue dialogue)
     {
+        ShowDialogueBox();
+        //convert single dialogue to an array contain only one dialogue
+        StartCoroutine(DisplayMultipleSentencesRoutine(new Dialogue[]{dialogue}));
+    }
 
+    public void ShowSentence(Dialogue[] dialogues)
+    {
+        ShowDialogueBox();
+        StartCoroutine(DisplayMultipleSentencesRoutine(dialogues));
+    }
+
+    public void ShowSentence(List<Dialogue> dialogues)
+    {
+        ShowDialogueBox();
+        StartCoroutine(DisplayMultipleSentencesRoutine(dialogues.ToArray()));
+    }
+
+    private IEnumerator DisplayMultipleSentencesRoutine(Dialogue[] dialogues)
+    {
+        //after player press space, show the next sentence
+        foreach(Dialogue dialogue in dialogues)
+        {
+            yield return DisplayOneSentenceRoutine(dialogue);
+            yield return new WaitUntil(()=>Input.GetButtonDown("Submit"));
+        }
+        HideDialogueBox();
+    }
+
+    private IEnumerator DisplayOneSentenceRoutine(Dialogue dialogue)
+    {
+        string head = "";
+        //if has speaker, add their name at the start
+        if(dialogue.speaker != null && dialogue.speaker.Length > 0)
+            head = dialogue.speaker + ":\n";
         for(int x = 1; x<= dialogue.content.Length;++x)
         {
             yield return null;
-            dialogueText.text = dialogue.name + "\n" + dialogue.content.Substring(0, x)
+            dialogueText.text = head + dialogue.content.Substring(0, x)
                                 + "<color=#ffffff00>" + dialogue.content.Substring(x) + "</color>";
         }
-        dialogueText.text = dialogue.name + "\n" + dialogue.content;
+        dialogueText.text = head + dialogue.content;
+    }
+
+    private void ShowDialogueBox()
+    {
+        dialogueBox.SetActive(true);
+    }
+
+    private void HideDialogueBox()
+    {
+        dialogueBox.SetActive(false);
     }
 
 }
