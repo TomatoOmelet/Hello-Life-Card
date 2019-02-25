f205e0e0de873a7f9380cf7eb732afd3fd67edaa
Blake Wakasa <bwakasa@uci.edu>
2019-02-24 19:59:33 -0800

    Added shop and complimentary scripts

---
+++ Hello Life Card/Assets/Scripts/Manager/ShopManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int money;//just used for testing purposes
+
+    public void makePurchase(int cost,string itemName)
+    {   
+        if (cost <= SystemManager.instance.playerMoney)
+        {
+            SystemManager.instance.playerMoney -= cost;
+            
+            //do the item action
+        }
+        else
+        {
+            //have a popup that says something to the effect of not enough money
+        }
+        //buys the item associated with the button
+
+    }
+
+
+}
