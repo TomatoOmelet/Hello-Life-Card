a1f76025c386245b6040b0c3247c3a11247bb790
Blake Wakasa <bwakasa@uci.edu>
2019-02-24 22:15:39 -0800

    Shop now works and has functionality (no images or specific items)

--- Hello Life Card/Assets/Scripts/Manager/ShopManager.cs
+++ Hello Life Card/Assets/Scripts/Manager/ShopManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,12 +7,29 @@ public class ShopManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public int money;//just used for testing purposes
+    public Dictionary<string,System.Action> items = new Dictionary<string,System.Action>();
 
-    public void makePurchase(int cost,string itemName)
-    {   
+    private void Start()
+    {
+        items.Add("Textbook", addIntelligence);
+    }
+
+    
+
+    public void makePurchase(string itemNC)
+    {//takes a string formatted in the form cost/itemname ex: 70/textbook/int/total
+        string strCost = "";
+        int i = 0; while(i<itemNC.Length && itemNC[i]!= '/')
+        {
+            strCost += itemNC[i];
+            ++i;
+            
+        }
+        int cost = int.Parse(strCost);
         if (cost <= SystemManager.instance.playerMoney)
         {
             SystemManager.instance.playerMoney -= cost;
+            items[itemNC.Substring(i + 1)]();
             
             //do the item action
         }
@@ -23,5 +41,11 @@ public class ShopManager : MonoBehaviour
 
     }
 
+    private void addIntelligence()
+    {
+        SystemManager.instance.playerIntelligence += 3;
+        Debug.Log("Player intel : "+SystemManager.instance.playerIntelligence);
+        Debug.Log("Player cash : " + SystemManager.instance.playerMoney);
+    }
 
 }
