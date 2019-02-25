1d88d0840eae5ff763471b6b71e32d2ccad35b8a
Weiyan Zhu <weiyanz@uci.edu>
2019-02-24 17:36:05 -0800

    Contacts Scriptable Objects

---
+++ Hello Life Card/Assets/Scripts/Social/ContactsData.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Contacts", menuName = "Hello/Contacts")]
+public class ContactsData : ScriptableObject
+{
+    public string name;
+    public Job job;
+}
