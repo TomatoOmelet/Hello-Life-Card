653bf159e147b4a03e823953efad5428a2febbc1
Weiyan Zhu <weiyanz@uci.edu>
2019-02-25 23:55:22 -0800

    put point at correct position

--- Hello Life Card/Assets/Scripts/ParticleEffect/ClickParticleManager.cs
+++ Hello Life Card/Assets/Scripts/ParticleEffect/ClickParticleManager.cs
@@ -1,23 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickParticleManager : MonoBehaviour
 {
-    public GameObject particleObject;
+    public Transform particleCanvas;
+    public CanvasScaler scaler;
+    public GameObject particleGenerater;
     public Camera myCamera;
+
+    void Start()
+    {
+
+    }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Input.mousePosition);
         //when player click, generate a particle
         if(Input.GetMouseButtonDown(0))
         {
-            Vector3 pos = myCamera.ScreenToWorldPoint(Input.mousePosition);
-            pos.z = -9;
-            GameObject particle = Instantiate(particleObject, pos, Quaternion.Euler(-90, 0, 0));
+            //Vector3 pos = myCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 pos = ScreenToCanvasPoint(Input.mousePosition);
+            Debug.Log(pos);
+            Debug.Log(Input.mousePosition);
+            //pos.z = -9;
+            GameObject particle = Instantiate(particleGenerater, pos, Quaternion.identity);
+            particle.transform.SetParent(particleCanvas, false);
             //particle.GetComponent<ParticleSystem>().Emit();
-            Destroy(particle, 2f);
+            //Destroy(particle, 2f);
         }
+    }
+
+    //transform a screen point to a canvas point
+    public Vector3 ScreenToCanvasPoint(Vector3 point)
+    {
+        //scale mouse to canvas
+        //get canvas size
+        float canvasX = (point.x - particleCanvas.transform.position.x) / particleCanvas.transform.localScale.x;
+        float canvasY = (point.y - particleCanvas.transform.position.y) / particleCanvas.transform.localScale.y;
+        return new Vector3(canvasX, canvasY, 0);
     }
 }
