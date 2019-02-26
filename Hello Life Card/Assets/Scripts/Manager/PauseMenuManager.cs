5852b23830e00d7ce2e438cf69a19be9a4f94f63
SupriseOrb <molinakmey@gmail.com>
2019-02-26 11:38:20 -0800

    Changed Main Page and created Pause Menu

---
+++ Hello Life Card/Assets/Scripts/Manager/PauseMenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    public GameObject pauseMenuPanel;
+
+    void Update()
+    {
+        if (Input.GetKey("escape"))
+        {
+            pauseMenuPanel.SetActive(true);
+        }
+    }
+
+    public void MainMenuButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void returnButton()
+    {
+        pauseMenuPanel.SetActive(false);
+    }
+}
