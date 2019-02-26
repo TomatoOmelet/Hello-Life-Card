f22b4cc9bdf0991ad7aa9cbf74b249fbf427b73d
Weiyan Zhu <weiyanz@uci.edu>
2019-02-25 18:24:05 -0800

    particle effect when clicking

---
+++ Hello Life Card/Assets/Scripts/EffectManager/ClickParticleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickParticleManager : MonoBehaviour
+{
+    public GameObject particleObject;
+    public Camera myCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //when player click, generate a particle
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = myCamera.ScreenToWorldPoint(Input.mousePosition);
+            pos.z = -9;
+            GameObject particle = Instantiate(particleObject, pos, Quaternion.Euler(-90, 0, 0));
+            //particle.GetComponent<ParticleSystem>().Emit();
+            Destroy(particle, 2f);
+        }
+    }
+}
