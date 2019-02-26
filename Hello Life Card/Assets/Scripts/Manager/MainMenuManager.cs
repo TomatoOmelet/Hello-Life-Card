5386b23deaff445faabdbbec46d972907855e417
SupriseOrb <molinakmey@gmail.com>
2019-02-25 20:43:28 -0800

    Added Instructions to Main Menu and Changed Build Settings

--- Hello Life Card/Assets/Scripts/Manager/MainMenuManager.cs
+++ Hello Life Card/Assets/Scripts/Manager/MainMenuManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public GameObject instructionPanel;
     public void StartButton()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene(1);
@@ -12,6 +13,16 @@ public class MainMenuManager : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    public void InstructionButton()
+    {
+        instructionPanel.SetActive(true);
+    }
+
+    public void returnButton()
+    {
+        instructionPanel.SetActive(false);
     }
 
 }
