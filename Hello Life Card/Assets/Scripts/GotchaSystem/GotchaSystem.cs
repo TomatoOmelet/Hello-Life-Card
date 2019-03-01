92d92c9f697d5906ac97a6e0721a201f1987ed40
Weiyan Zhu <weiyanz@uci.edu>
2019-02-28 23:21:54 -0800

    basic animation

--- Hello Life Card/Assets/Scripts/GotchaSystem/GotchaSystem.cs
+++ Hello Life Card/Assets/Scripts/GotchaSystem/GotchaSystem.cs
@@ -7,25 +7,38 @@ public class GotchaSystem : MonoBehaviour
 {
     public JobHunt jobHunt;
     public GameObject resume;
+    public GameObject result;
+    public Transform resumeLocation;
+    public Transform recruiterLocation;
+
+    private Job job;
+
+    void Start()
+    {
+        Reset();
+    }
 
     //====================================================================================
     //System
     //====================================================================================
     public void Reset()
     {
+        job = null;
         //reset resume
-        resume.transform.position = new Vector3(0, 0, resume.transform.position.z);
+        resume.transform.position = resumeLocation.position;
         resume.GetComponent<Button>().interactable = true;
         //reset location of result    
+        result.transform.position = recruiterLocation.position; 
+        result.GetComponent<Button>().interactable = false;
     }
 
     public void Gotcha()
     {
         //set button to not interactable
         resume.GetComponent<Button>().interactable = false;
-        Job job = jobHunt.Hunt();
+        job = jobHunt.Hunt();
 
-        //StartCoroutine(GotchaAnimation(job));
+        StartCoroutine(GotchaAnimation(job));
         //if get no job
         if(job == null)
         {
@@ -35,14 +48,33 @@ public class GotchaSystem : MonoBehaviour
         }
     }
 
-    //public IEnumerator GotchaAnimation(Job job)
-    //{
+    public IEnumerator GotchaAnimation(Job job)
+    {
+        float speed = 1300f;
+        speed *= Time.deltaTime;
         //move resume up
+        for(float y = resumeLocation.position.y; y < recruiterLocation.position.y; y += speed)
+        {
+            Vector3 newPosition = resumeLocation.position;
+            newPosition.y = y;
+            resume.transform.position = newPosition;
+            yield return new WaitForSeconds(0.02f);
+        }
+        resume.transform.position = recruiterLocation.position;
+        //wait for a while 
+        //yield return new WaitForSeconds(2f);
+        //move result down
+        for(float y = recruiterLocation.position.y; y > resumeLocation.position.y ; y -= speed)
+        {
+            Vector3 newPosition = recruiterLocation.position;
+            newPosition.y = y;
+            result.transform.position = newPosition;
+            yield return new WaitForSeconds(0.02f);
+        }
+        result.transform.position = resumeLocation.position;
+        result.GetComponent<Button>().interactable = true;
         
-        //move things down
-
-        
-    //}
+    }
 
     //====================================================================================
     //UI 
@@ -51,11 +83,21 @@ public class GotchaSystem : MonoBehaviour
     public void OpenPage()
     {
         gameObject.SetActive(true);
+        Reset();
     }
 
     public void ClosePage()
     {
         gameObject.SetActive(false);
+    }
+
+    public void OpenResult()
+    {
+        if(job != null)
+        {
+            jobHunt.SetupJobOffer(job);
+        }
+        Reset();
     }
 
 }
