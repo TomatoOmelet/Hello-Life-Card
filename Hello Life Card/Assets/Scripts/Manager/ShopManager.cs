e79557f1c2b7120ba1fa2a4b09f3d564f6a2cc4f
Blake Wakasa <bwakasa@uci.edu>
2019-03-03 17:25:01 -0800

    Adding parts of item UI prefab and Shop Panel

--- Hello Life Card/Assets/Scripts/Manager/ShopManager.cs
+++ Hello Life Card/Assets/Scripts/Manager/ShopManager.cs
@@ -6,20 +6,21 @@ using UnityEngine;
 public class ShopManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    private int uiIntelligence;//just used for testing purposes
-    private int uiMoney = 0;
+   // private int uiIntelligence;//just used for testing purposes
+    //private int uiMoney = 0;
     public RNGGenerator lottery;
-
+    
     public Dictionary<string,System.Action> items = new Dictionary<string,System.Action>();
     //RNGGenerator rng = new RNGGenerator();
    
     private void Start()
     {
+        //SystemManager.instance.playerMoney = 2000;
 
-        Debug.Log(SystemManager.instance.playerMoney);
+      //Debug.Log(SystemManager.instance.playerMoney);
         items.Add("Textbook", getTextbook);
         items.Add("Fancy Desk", studyRate);
-        //items.Add("Fancy Cell Phone", addContacts);
+        items.Add("Fancy Cell Phone", addContacts);
         items.Add("Lottery Ticket", rollLotterySmall);
         items.Add("Fancy Lottery Ticket", rollLotteryMedium);
         items.Add("Nothing", doNothing);
@@ -30,6 +31,9 @@ public class ShopManager : MonoBehaviour
         items.Add("500", gainAMoney);
         items.Add("1000", gainSMoney);
     }
+
+    
+
 
     private void gainSMoney()
     {
@@ -58,6 +62,8 @@ public class ShopManager : MonoBehaviour
     public void makePurchase(string itemNC)
     {//takes a string formatted in the form cost/itemname ex: 70/textbook/int/total
         string strCost = "";
+//      Debug.Log(itemNC);
+ //     Debug.Log(SystemManager.instance.playerMoney);
         int i = 0; while(i<itemNC.Length && itemNC[i]!= '/')
         {
             strCost += itemNC[i];
@@ -65,20 +71,23 @@ public class ShopManager : MonoBehaviour
             
         }
         int cost = int.Parse(strCost);
+
         if (cost <= SystemManager.instance.playerMoney)
         {
+            string itemname = itemNC.Substring(i + 1);
 
-            Debug.Log("this is the cost: "+cost);
-            //Debug.Log(itemNC.Substring(i + 1));
-            //StartCoroutine(SubtractCash(cost));
-            items[itemNC.Substring(i + 1)]();
-            StartCoroutine(AdjustCash(cost,-1));
-            //Debug.Log(SystemManager.instance.playerMoney + " total money");
             
-            //do the item action
+            StartCoroutine(handleDialog(itemname));
+            
+
+            //items[itemNC.Substring(i + 1)]();
+            AdjustCash(-1*cost);
+
         }
         else
         {
+            
+            StartCoroutine(display(new Dialogue("", "Unfortunately, you don't have the proper funding")));
             //have a popup that says something to the effect of not enough money
         }
         //buys the item associated with the button
@@ -87,8 +96,10 @@ public class ShopManager : MonoBehaviour
 
     private void getTextbook()
     {
+
+       //ystemManager.instance.playerIntelligence += 30;
         Debug.Log("After receiving a textbook, you feel like you've gotten smarter");
-        StartCoroutine(IntelIncrease(30, (new Dialogue("", "With a textbook, you feel smarter. Your intelligence increases by " + 30 + " ."))));
+       
         
         
     }
@@ -99,42 +110,128 @@ public class ShopManager : MonoBehaviour
     {
         Debug.Log("With a new desk, you will be better at studying");
     }
-   // private void addContacts()
-    //{
-
-    //}
-
-    private void rollLotterySmall()
+   private void addContacts()
     {
-        StartCoroutine(display(new Dialogue("", "You have received a Normal Lottery Ticket, best of luck!")));
-        string str = lottery.GenerateCPrize();
-        items[str]();
+
     }
+
+
+    private void  rollLotterySmall()
+    {
+        //StartCoroutine(display(new Dialogue("", "You have received a Normal Lottery Ticket, best of luck!")));
+        string str = lottery.GenerateCPrize();
+        StartCoroutine(handleDialog(str));
+        //items[str]();
+    }
+    
+
     private void rollLotteryMedium()
     {
-        StartCoroutine(display(new Dialogue("", "You have received a Fancy Lottery Ticket, best of luck!")));
+        //StartCoroutine(display(new Dialogue("", "You have received a Fancy Lottery Ticket, best of luck!")));
         string str = lottery.GenerateBPrize();
-        items[str]();
-
+        StartCoroutine(handleDialog(str));
+        //items[str]();
+        
     }
     private void doNothing()
     {
-        StartCoroutine(display (new Dialogue("", "You have gained nothin from this exchange")));
+        //StartCoroutine(display (new Dialogue("", "You have gained nothing from this exchange")));
         Debug.Log("You get nothing ");
     }
     private void rollLotteryLarge()
     {
-        StartCoroutine(display(new Dialogue("", "You have received an Extreme Lottery Ticket, best of luck!")));
+        //StartCoroutine(display(new Dialogue("", "You have received an Extreme Lottery Ticket, best of luck!")));
         string str = lottery.GenerateAPrize();
-        items[str]();
+        StartCoroutine(handleDialog(str));
+        //items[str]();
     }
     private void addLifeCardFragment()
     {
+        SystemManager.instance.playerLifeCardFragment++;
         Debug.Log("Life is in your grasp");
+    }
+
+    public IEnumerator handleDialog(string itemName)
+    {
+      Debug.Log("got here " + itemName);
+        if (itemName == "Textbook")
+        {
+        //  Debug.Log("this is dumb");
+            yield return display(new Dialogue("", "After receiving a textbook, you feel like you've gotten smarter"));
+            items[itemName]();
+            StartCoroutine(AddScore(30));
+        }
+       else if(itemName =="Fancy Desk")
+        {
+        //  Debug.Log("this is fancy desk");
+             yield return display(new Dialogue("", "With a new desk, you will be better at studying"));
+            items[itemName]();
+        }
+        else if(itemName =="Fancy Cell Phone")
+        {
+        //  Debug.Log("this is fancy cell phone");
+            yield return null; //display(new Dialogue "", "")
+            items[itemName]();
+        }
+        else if (itemName =="Lottery Ticket")
+        {
+        //  Debug.Log("this is Lottery Ticket");
+            yield return display(new Dialogue("", "You have received a Normal Lottery Ticket, best of luck!"));
+            items[itemName]();
+        }
+        else if (itemName =="Fancy Lottery Ticket")
+        {
+         // Debug.Log("this is Fancy Lottery Ticket");
+            yield return display(new Dialogue("", "You have received a Fancy Lottery Ticket, best of luck!"));
+            items[itemName]();
+        }
+        else if (itemName =="Extreme Lottery Ticket")
+        {
+         // Debug.Log("this is Extreme Lottery Ticket");
+            yield return display(new Dialogue("", "You have received a Fancy Lottery Ticket, best of luck!"));
+            items[itemName]();
+        }
+        else if (itemName== "Nothing")
+        {
+            Debug.Log("this is nothing");
+            yield return display(new Dialogue("", "You have gained nothing from this exchange"));
+        }
+        else if (itemName == "10")
+        {
+            yield return display(new Dialogue("", "You have received 10 dollars!"));
+
+            AdjustCash(10);
+        }
+        else if (itemName == "100")
+        {
+            yield return display(new Dialogue("", "You have received 100 dollars!"));
+            AdjustCash(100);
+        }
+        else if(itemName == "500")
+        {
+            yield return display(new Dialogue("", "You have received 500 dollars!"));
+            AdjustCash(500);
+
+        }
+        else if (itemName == "1000")
+        {
+            yield return display(new Dialogue("", "You have received 1000 dollars!"));
+            AdjustCash(1000);
+        }
+        
+        //yield return display(new Dialogue("",""));
+        //yield return display(new Dialogue("",""));
+        //yield return display(new Dialogue("",""));
+        //yield return display(new Dialogue("",""));
+        //yield return display(new Dialogue("",""));
+        //yield return display(new Dialogue("",""));
+        //yield return display(new Dialogue("",""));
+        //yield return display(new Dialogue("",""));
     }
 
 public IEnumerator display(Dialogue dialogue)
     {
+        //Debug.Log(dialogue.content + " this is the dialog content");
         yield return SystemManager.instance.dialogueManager.DisplaySentence(dialogue);
     }
 
@@ -150,29 +247,40 @@ public IEnumerator IntelIncrease(int amount,Dialogue dialogue)
 
 public IEnumerator AddScore(int value)
     {
-        SystemManager.instance.playerIntelligence += value;
-        //SystemManager.instance.uiManager.PromptIntelligenceChange(value);
+        //SystemManager.instance.playerIntelligence += value;
+      //SystemManager.instance.uiManager.PromptIntelligenceChange(value);
         //add numbers one by one
-        for(int x = 0; x< value; ++x)
-        {
-            SystemManager.instance.uiManager.UpdateIntelligenceUI(++uiIntelligence);
-            yield return null;
-        }
-    }
-    public IEnumerator AdjustCash(int value,int sign)
-    {
-        Debug.Log("TOTAL AMOUNT OF MONEY" + SystemManager.instance.playerMoney);
         
-        SystemManager.instance.playerMoney += (value*sign);
+        //construc the sentence displayed before 
+       
+       //ield return SystemManager.instance.dialogueManager.DisplaySentence(dialogue);
+        StartCoroutine(SystemManager.instance.uiManager.AddValueToUI(SystemManager.instance.uiManager.intelligenceText, value));
+        yield return null;
+      //StartCoroutine(SystemManager.instance.uiManager.AddValueToUI(SystemManager.instance.uiManager.intelligenceText, value));
+        //syield return SystemManager.instance.uiManager.AddValueToUI(SystemManager.instance.uiManager.intelligenceText, value);
+            //yield return null;
+        
+    }
+    public void AdjustCash(int value)
+    {
+      //Debug.Log("TOTAL AMOUNT OF MONEY" + SystemManager.instance.playerMoney);
+        
+      //SystemManager.instance.playerMoney-= (value);
         //SystemManager.instance.uiManager.PromptIntelligenceChange(value);
         //add numbers one by one
-        Debug.Log(SystemManager.instance.playerMoney);
-        for (int x = 0; x < value; ++x)
-        {
+     // Debug.Log(SystemManager.instance.playerMoney);
+        
+        
             //UPDATE UI
-            SystemManager.instance.uiManager.UpdateMoneyUI(--uiMoney);
-            yield return null;
-        }
+        StartCoroutine(SystemManager.instance.uiManager.AddValueToUI(SystemManager.instance.uiManager.moneyText, value));
+        // yield return null;
+       
+
+        //int intelligence = IntelligenceIncrease();
+        //construc the sentence displayed before 
+        //Dialogue dialogue = new Dialogue("", "After studying hard, your intelligence increases by " + intelligence + " .");
+        //yield return SystemManager.instance.dialogueManager.DisplaySentence(dialogue);
+        //StartCoroutine(SystemManager.instance.uiManager.AddValueToUI(SystemManager.instance.uiManager.intelligenceText, intelligence));
     }
 
     
