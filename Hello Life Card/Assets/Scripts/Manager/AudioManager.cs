4a62ca5f37797b82c3af212651817f33527a6a75
Kyle Jin Jones <kjjones1@uci.edu>
2019-03-19 07:02:20 -0700

    Button clicks and nicer instructions

--- Hello Life Card/Assets/Scripts/Manager/AudioManager.cs
+++ Hello Life Card/Assets/Scripts/Manager/AudioManager.cs
@@ -5,14 +5,15 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    AudioSource a;
     void Start()
     {
-        
+        a = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayClip(AudioClip clip)
     {
-        
+        a.clip = clip;
+        a.Play();
     }
 }
